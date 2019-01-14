9dadafc2f6093c6accc6836f74e081116c7eeb54
Jorge Diaz <jorge7soccer@gmail.com>
2019-01-14 14:30:54 -0500

    Adding first C program to repo.

---
+++ Hello/Hello/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Hello
+//
+//  My first C program.
+//
+//  Created by Jorge Diaz on 1/14/19.
+//  Copyright © 2019 Jorge Diaz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
